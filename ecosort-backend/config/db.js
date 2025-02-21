d8b292e07405e4cbd08802475027c6e7fc6032d7
DimaDD <dimatruba2004@yandex.ru>
2025-02-21 03:12:11 +0300

    docker

--- ecosort-backend/config/db.js
+++ ecosort-backend/config/db.js
@@ -1,30 +1,49 @@
-const sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 
-global.sequelize = new sequelize(
-    'ecosort', 
-    'root', 
-    '1111', 
-    {host:'localhost', dialect:'mysql'})
+const isDocker = process.env.DOCKER_ENV === 'true';
 
-// докер
-// global.sequelize = new sequelize(
-// 'ecosort', 
-// 'root', '1111', 
-// {host:'host.docker.internal', dialect:'mysql'})
+const dbConfig = {
+  database: 'ecosort',
+  username: 'root',
+  password: '1111',
+  host: isDocker ? 'host.docker.internal' : 'localhost',
+  dialect: 'mysql',
+};
 
-const {Articles} = require('./Articles')
-const {Discounts} = require('./Discounts')
-const {Marks} = require('./Marks')
-const {Points} = require('./Points')
-const {Ratings} = require('./Ratings')
-const {Receptions} = require('./Receptions')
-const {Users} = require('./Users')
-const {Keys} = require('./Keys')
-const {Check_weights} = require('./Check_weights')
-const {Likes} = require('./Likes')
-const {Points_marks} = require('./Points_marks')
-const {Promo_codes} = require('./Promo_codes')
+// Инициализация Sequelize
+global.sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  host: dbConfig.host,
+  dialect: dbConfig.dialect,
+});
 
+// Импорт моделей
+const { Articles } = require('./Articles');
+const { Discounts } = require('./Discounts');
+const { Marks } = require('./Marks');
+const { Points } = require('./Points');
+const { Ratings } = require('./Ratings');
+const { Receptions } = require('./Receptions');
+const { Users } = require('./Users');
+const { Keys } = require('./Keys');
+const { Check_weights } = require('./Check_weights');
+const { Likes } = require('./Likes');
+const { Points_marks } = require('./Points_marks');
+const { Promo_codes } = require('./Promo_codes');
+
+// Экспорт моделей
 module.exports = {
-    models: {Articles, Discounts, Marks, Points, Ratings, Receptions, Users, Keys, Check_weights, Likes, Points_marks, Promo_codes}
-}
+  models: {
+    Articles,
+    Discounts,
+    Marks,
+    Points,
+    Ratings,
+    Receptions,
+    Users,
+    Keys,
+    Check_weights,
+    Likes,
+    Points_marks,
+    Promo_codes,
+  },
+};
